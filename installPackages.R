fcd5a8c834b41f00d5259d30874c9ef7640abfad
Jeffrey Doto <jeffdoto@amazon.com>
2021-02-18 12:23:22 -0500

    Dockerizing report...still working out dependency issues.

--- installPackages.R
+++ installPackages.R
@@ -3,3 +3,5 @@ library('devtools')
 # https://github.com/r-lib/devtools/issues/647
 options(unzip = 'internal')
 install_github("everettJK/gt23")
+install.packages('backports', repos='http://cran.us.r-project.org')
+install.packages('checkmate', repos='http://cran.us.r-project.org')
